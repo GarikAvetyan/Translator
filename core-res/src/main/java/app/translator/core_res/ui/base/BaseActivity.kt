7059221c8a962092325cd88888a4a7eb911342cd
GarikAvetyan <garik.avetyan201@gmail.com>
2026-03-13 18:25:12 +0400

    Initial commit: project setup with basic structure and configuration

---
+++ core-res/src/main/java/app/translator/core_res/ui/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package app.translator.core_res.ui.base
+
+import androidx.activity.ComponentActivity
+
+open class BaseActivity : ComponentActivity()
---
+++ core-res/src/main/java/app/translator/core_res/ui/base/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package app.translator.core_res.ui.base
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel()
